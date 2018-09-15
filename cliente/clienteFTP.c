ee47cb748c72a38aa35385e36f8092b02929c9d6
root <gbrabosa@gmail.com>
2018-09-15 00:55:01 -0300

    Modifica servidor e cliente. Já estão comunicando o nome do arquivo.

--- cliente/clienteFTP.c
+++ cliente/clienteFTP.c
@@ -1,43 +1,76 @@
 /*
-Strem sockets
-client-server
-Host: localhost
-2-tier architecture
-Iteractive server
-IP 127.0.0.1
-Client:
+Client
 
 create a socket with socket()
 connect the socket to the address of the server connect()
 send and receive data using  read() write()
 
-Server:
+*/
 
-Create a socket with the socket()
-Bind the socket to an address using the bind() (hostname:port)
-Listen for connections with the listen()
-Accept a connection with the accept()
-Send and receive data using the read() and write()*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-//argv[0] = host_do_servidor
-//argv[1] = porto_do_servidor
-//argv[2] = nome_arquivo
-//argv[3] = tam_buffer
+//argv[1] = host_do_servidor
+//argv[2] = porto_do_servidor
+//argv[3] = nome_arquivo
+//argv[4] = tam_buffer
 
 
 int main(int argc, char *argv[]) {
+
+  //Salva o tempo inicial
   struct timeval start, end;
   gettimeofday(&start, NULL);
+  //Processa entradas
+  int tam_buffer = atoi(argv[4]);
+  int port = atoi(argv[2]);
+  printf("%d\n",port);
+  char buffer[tam_buffer];
   //gettimeofday(&end, NULL);
-  char hostname[] = "localhost";
-  printf("%s\n",hostname);
+
+  //Cria a socket
+  int socketid;
+  socketid = socket(AF_INET,SOCK_STREAM,0);
+
+  //Especifica o endereço do socket do cliente
+  struct sockaddr_in server_adress;
+  server_adress.sin_family = AF_INET;
+  server_adress.sin_port = htons(port);//passa o número da porta
+  server_adress.sin_addr.s_addr = INADDR_ANY;
+  //server_adress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  //conecta ao servidor
+  int status = connect(socketid, (struct sockaddr *) &server_adress, sizeof(server_adress));
+  if (status == -1){
+    printf("Erro ao estabelecer a conexao\n");
+  }
+  else
+    printf("status = %d, conexao estabelecida\n",status);
+
+  //envia nome_arquivo
+  if (strlen(argv[3]) >= tam_buffer)
+    printf("%s is too long!\n",argv[3]);
+  else
+    strcpy(buffer, argv[3]);
+
+  int n;
+  n = write(socketid,buffer,255);
+  printf("n = %d\n",n);
+  //send(socketid, buffer, sizeof(buffer), 0);
+
+
+  close(socketid);
+
 
   return 0;
-  }
+
+}
--- servidor/servidorFTP.c
+++ servidor/servidorFTP.c
@@ -1,10 +1,68 @@
+/*
+Server:
+
+Create a socket with the socket()
+Bind the socket to an address using the bind() (hostname:port)
+Listen for connections with the listen()
+Accept a connection with the accept()
+Send and receive data using the read() and write()
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
-int main(int argc, char *argv[]) {
+#include <unistd.h>
+#include <arpa/inet.h>
 
-return 0;
+//argv[1] = porta_servidor
+//argv[2] = tam_buffer
+int main(int argc, char *argv[]) {
+  //processa os argumentos
+  int tam_buffer = atoi(argv[2]);
+  int port = atoi(argv[1]);
+  char buffer[tam_buffer];
+
+  //cria o socket
+  int server_socket;
+  server_socket = socket(AF_INET,SOCK_STREAM,0);
+
+  //Especifica o socket do server
+  struct sockaddr_in server_adress;
+  server_adress.sin_family = AF_INET;
+  server_adress.sin_port = htons(port);//passa o número da porta
+  server_adress.sin_addr.s_addr = INADDR_ANY;
+
+  //server_adress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  //Cria o bind para a porta e o ip
+  bind(server_socket, (struct sockaddr *) &server_adress, sizeof(server_adress));
+
+  //Faz abertura passiva e aguarda por conexao
+  listen(server_socket, 5);
+
+  //Aceita conexao
+  int client_socket;
+  client_socket = accept(server_socket, NULL, NULL);
+  if (client_socket < 0) {
+   perror("ERROR on accept");
+   exit(1);
+}
+  //recebe nome do arquivo
+  char nome_arquivo[tam_buffer];
+  //recv(client_socket, &nome_arquivo, sizeof(nome_arquivo), 0);
+  int n;
+  n = read(client_socket,nome_arquivo,sizeof(nome_arquivo) );
+  printf("%s\n", nome_arquivo);
+  //envia arquivo
+  //send(client_socket, buffer, sizeof(buffer), 0);
+
+  close(server_socket);
+
+  return 0;
 }
